b6498fc15f820b12c75a368b48c7d43dce89f3a3
Jane Lee <janelee1721@gmail.com>
2025-04-23 17:30:18 +0900

    🎉Init: Project setup

---
+++ src/main/java/com/jane/objecttheater/ObjectTheaterApplication.java
@@ -0,0 +1,13 @@
+package com.jane.objecttheater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectTheaterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObjectTheaterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jane/objecttheater/ObjectTheaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jane.objecttheater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectTheaterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
